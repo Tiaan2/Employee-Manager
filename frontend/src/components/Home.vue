a482b4cb6752a30e04ccfbc9cb44a512efb814a0
Tiaan Pouwels <97939187+Tiaan2@users.noreply.github.com>
2024-08-14 16:58:25 +0200

    Datatable initialised

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,7 +1,48 @@
 <script setup lang="ts">
-import Home from './Home.vue'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import { collection, getDocs, Timestamp } from 'firebase/firestore'
+import { db } from '../../firebaseconfig'
+import { ref, onMounted } from 'vue'
+
+type Employee = {
+  name: string
+  surname: string
+  birthDate: Timestamp
+  employeeNumber: string
+  salary: number
+  position: string
+  isManager: boolean
+}
+
+const employees = ref<Employee[]>([])
+
+const fetchData = async () => {
+  try {
+    const querySnapshot = await getDocs(collection(db, 'employees'))
+    employees.value = querySnapshot.docs.map((doc) => doc.data() as Employee)
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
+
+onMounted(() => {
+  fetchData()
+})
 </script>
 
-<template></template>
+<template>
+  <div>
+    <DataTable :value="employees" :paginator="true" :rows="10" responsive-layout="scroll">
+      <Column field="name" header="Name" />
+      <Column field="surname" header="Surname" />
+      <Column field="birthDate" header="Birth Date" />
+      <Column field="employeeNumber" header="Employee Number" />
+      <Column field="salary" header="Salary" />
+      <Column field="position" header="Role" />
+      <Column field="isManager" header="Reporting Line" />
+    </DataTable>
+  </div>
+</template>
 
 <style scoped></style>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,10 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  optimizeDeps: {
+    include: ['primevue/datatable', 'primevue/column']
+  },
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
