74ec5904746fb53758997dbd2933a7958154654f
Tiaan Pouwels <97939187+Tiaan2@users.noreply.github.com>
2024-08-26 22:04:51 +0200

    Updated API key and made more secure

--- frontend/firebaseconfig.ts
+++ frontend/firebaseconfig.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 import { ref } from 'vue'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDUlADOwYoO5DM3raOwyFVmdqpGgNnBiUA',
-  authDomain: 'epi-use-project.firebaseapp.com',
-  projectId: 'epi-use-project',
-  storageBucket: 'epi-use-project.appspot.com',
-  messagingSenderId: '294731759929',
-  appId: '1:294731759929:web:1b2a3441a92cfc297ae8e8',
-  measurementId: 'G-LHH3QH2HWT'
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
